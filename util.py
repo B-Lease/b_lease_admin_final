0e11df6f070010585ba45823a6da1b678b7010fd
allainzenith <allainzenith12@gmail.com>
2023-03-26 21:25:03 +0800

    undo last commit

--- util.py
+++ util.py
@@ -53,7 +53,7 @@ def createPDF(leasingID:str):
     #one line is cell, multi-line is multi_cell
     #width = 0 (sets the width to the entire page)
     #width 40, length 10, content of cell
-    pdf.cell(40,10,'This is a contract')
+    pdf.cell(40,10,'Hello World!')
 
     directory = f"static\\{leasingID}"
 
