3080f44d5bbba480dd383bd48bbc283d6f967c1c
NATHANIEL TIEMPO <tiemponathaniel@gmail.com>
2023-04-13 16:21:08 +0800

    Merged kyla and main admin

--- util.py
+++ util.py
@@ -37,6 +37,28 @@ class JSONEncoder(json.JSONEncoder):
         else:
             return super().default(z)
         
+def createPDF(leasingID:str):
+    #portrait layout, mm unit of measurement, and letter format (short bp)
+    pdf = FPDF('P', 'mm', 'Letter')
+    pdf.add_page()
+    
+    #regular helvetica and 16 font-size
+    pdf.set_font('helvetica','',16)
+
+    #one line is cell, multi-line is multi_cell
+    #width = 0 (sets the width to the entire page)
+    #width 40, length 10, content of cell
+    pdf.cell(40,10,'Hello World!')
+
+    directory = f"static\\{leasingID}"
+
+    # Create the directory if it doesn't exist
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+    filename = generateUUID(leasingID)
+    pdf.output(f"static\\{leasingID}\{filename}_contract.pdf")
+    return filename
 
 def convertPDFasBlob(file_path:str, leasing_doc_name:str):
     # Open the PDF file
@@ -46,4 +68,5 @@ def convertPDFasBlob(file_path:str, leasing_doc_name:str):
 
     # Return the file as a blob
     return send_file(file_object, attachment_filename=leasing_doc_name, as_attachment=True)
+
 
